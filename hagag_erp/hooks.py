e8ed761e275256a716dd65f195a8a8ae4eb16ed8
root <root@vmi3571633.contaboserver.net>
2026-09-25 01:01:06 +0200

    feat: Hagag UI/UX v9 (pure css rewrite, revert bundle)

--- hagag_erp/hooks.py
+++ hagag_erp/hooks.py
@@ -265,6 +265,6 @@ app_license = "mit"
 
 after_request = ["hagag_erp.hrms_inject.inject"]
 
-app_include_css = ["/assets/hagag_erp/css/hagag_desk.bundle.css"]
+app_include_css = ["/assets/hagag_erp/css/hagag_desk.css"]
 
-web_include_css = ["/assets/hagag_erp/css/hagag_web.bundle.css"]
+web_include_css = ["/assets/hagag_erp/css/hagag_web.css"]
